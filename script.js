1fdc430533a4cac0f182738e2a60e8543edb58f5
EllaMiri <ella.larsson0012@gmail.com>
2021-11-17 15:44:25 +0100

    added button, eventlisteners and run function

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = main;
+
+let inventory = [];
+
+function main () {
+    addEventListeners()
+}
+
+function addEventListeners() {
+    let startGame = document.getElementById("startbutton")
+    startGame.onclick = run;
+}
+
+function run() {
+    alert("Välkommen till Hello World! Detta spel går ut på att du är en karaktär från planeten Hello World och du ska bekämpa ett monster som kommit från planeten Goodbye World. Monstret försöker förstöra din planet.")
+    alert("För att bekämpa monstret måste du ha ett svärd och en sköld. Dessa hittar du genom att utforska världen. Lycka till!")
+    firstStage();
+}
