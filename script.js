68ea82dfe2c4d0b8e6123e566ef28cebb0e3b44f
EllaMiri <ella.larsson0012@gmail.com>
2021-11-17 15:48:16 +0100

    added forest part of story

--- script.js
+++ script.js
@@ -31,3 +31,66 @@ function firstStage() {
         firstStage()
     }
 }
+
+function forest() {
+    alert("Du går in i skogen. Du ser ett hus framför dig. Du knackar på dörren.")
+        alert("En gubbe öppnar. Han frågar vad du vill och du svarar att du letar efter ett svärd, en sköld och en hjälm. Gubben säger att han nog har ett svärd hemma men han minns inte vart han lagt det... Du kan gärna leta här inne säger han.")
+        insideHouse();
+}
+
+function insideHouse() {
+    let openChest = prompt("I hallen ser du en kista. Vill du öppna den? Ange ja eller nej")
+    if (openChest === "ja") {
+        alert("Kistan är tom.. Leta vidare")
+    }
+    else if (openChest === "nej"){
+        alert("Du letar vidare")
+    }
+    else {
+        alert("Vänligen ange ja eller nej")
+        insideHouse()
+    }
+    upstairs()
+}
+
+function upstairs() {
+    let openWardrobe = prompt("Du ser en trappa. Du går upp på övervåningen. Där uppe i ett av rummen ser du en garderob. Vill du öppna den? Ange ja eller nej.")
+    
+    if (openWardrobe === "ja") {
+        alert("Där ligger svärdet! Du har hittat det. Nu saknas bara skölden.")
+        inventory.push("Svärd")
+        let whereNext = prompt("Vart vill du gå nu? Ange fight för att gå och bekämpa monstret eller grottan för att gå till grottan")
+
+        if (whereNext === "grottan"){
+            goToCave();
+        }
+        else if (whereNext === "fight") {
+            alert("Oops, du har ju inte skölden än... Du måste hämta den först. Klicka ok för att gå till grottan.")
+            goToCave()
+        }
+        else {
+            alert("Vänligen ange fight eller grottan")
+            upstairs()
+        }
+        
+    }
+    else if(openWardrobe === "nej") {
+        alert("Gubben kommer upp till dig på övervåningen. Han sa att han minns nu var han lagt svärdet. Det ligger i garderoben säger han och öppnar den. Han ger dig svärdet. Nu behöver du bara skölden.")
+        inventory.push("Svärd")
+        let whereNext = prompt("Vart vill du gå nu? Ange fight för att gå och bekämpa monstret eller grottan för att gå till grottan")
+
+        if (whereNext === "grottan"){
+            goToCave();
+        }
+        else if (whereNext === "fight") {
+            alert("Oops, du har ju inte skölden än... Du måste hämta den först. Klicka ok för att gå till grottan.")
+            goToCave()
+        }
+        else {
+            alert("Vänligen ange fight eller grottan")
+            upstairs()
+        }
+
+    }
+    
+}
